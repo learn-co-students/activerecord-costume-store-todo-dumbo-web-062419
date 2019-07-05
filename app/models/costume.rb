de6b1d6199aaa44ffbe828ccfb45e7982da0448d
github username <github email address>
2019-07-05 15:55:44 -0400

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,7 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costume < ActiveRecord::Base
+    belongs_to :haunted_house
+    belongs_to :costume_store
+end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,5 @@
 # Create your HauntedHouse class here
+class HauntedHouse < ActiveRecord::Base
+    has_many :costumes
+    has_many :costume_stores, through: :costumes
+end
